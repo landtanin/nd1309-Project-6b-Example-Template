aa4236482ddaf3b137fdfea3978df887f4a45af4
Tanin <landtanin@gmail.com>
2021-11-26 20:49:17 +0000

    Add hdwallet and infura connections

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,10 +1,25 @@
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/6e51863fffa84e40a35843166395dbd2`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
+  },
+  mocha: {
+    timeout: 20000
   },
   compilers: {
     solc: {
